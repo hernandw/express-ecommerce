298ed763f776b2c616eb4b65388866eb6288bd89
Williams Hernandez <hernandw@gmail.com>
2023-06-27 20:33:23 -0400

    :sparkles: consultas por parametros

--- consultas.js
+++ consultas.js
@@ -1,4 +1,5 @@
 const pool = require('./db/conexion.js');
+const format = require('pg-format');
 
 
 /* const getProducts = async (req, res) => {
@@ -14,10 +15,47 @@ const getProducts = async ({limits = 10}) => {
     const { rows: productos } = await pool.query(consulta, [limits])
     return productos
 }
+//ordenar por campso ASC o DESC
+const getProductOrder = async ({ limits = 10, order_by = "price_asc" }) => {
+    const [campo, direccion] = order_by.split("_");
+    const formatQuery = format("SELECT * FROM products ORDER BY %s %s LIMIT %s", campo, direccion, limits);
+    const { rows: productos } = await pool.query(formatQuery);
+    return productos
+    
+}
+
+//paginacion de registros
+/* const getProductLimitPage = async ({ page = 0, limit = 10, order_by = "price_asc" }) => {
+    const [campo, direccion] = order_by.split("_");
+    const offset = page * limit;
+    const formatQuery = format("SELECT * FROM products ORDER BY %s %s LIMIT %s OFFSET %s", campo, direccion, limit, offset);
+    const { rows: productos } = await pool.query(formatQuery);
+    return productos
+} */
+
+const getProductLimitPage = async ({
+  page = 1,
+  limit = 10,
+  order_by = "id_asc",
+}) => {
+  const [campo, direccion] = order_by.split("_");
+  const offset = (page - 1) * limit;
+  const formatQuery = format(
+    "SELECT * FROM products ORDER BY %s %s LIMIT %s OFFSET %s",
+    campo,
+    direccion,
+    limit,
+    offset
+  );
+  const { rows: productos } = await pool.query(formatQuery);
+  return productos;
+};
 
 
 
 module.exports = {
-    getProducts
+    getProducts,
+    getProductOrder,
+    getProductLimitPage
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts} = require("../consultas.js");
+const { getProducts, getProductOrder, getProductLimitPage } = require("../consultas.js");
 
 router.get("/", (req, res) => {
-    res.send("Hello World en Express desde Router!");
-})
-
-router.get("/consultas", (req, res) => {
-    res.send("Hello World en Consultas desde Router!");
-})
-
-
-router.get('/products', async(req, res)=> {
-    const queryStrings = req.query;
-    const products = await getProducts(queryStrings);
-    res.json(products);
+  res.send("Hello World en Express desde Router!");
 });
 
-module.exports = router
+router.get("/consultas", (req, res) => {
+  res.send("Hello World en Consultas desde Router!");
+});
+
+router.get("/products", async (req, res) => {
+  const queryStrings = req.query;
+  const products = await getProducts(queryStrings);
+  res.json(products);
+});
+
+router.get("/products/order", async (req, res) => {
+  const queryStrings = req.query;
+  const products = await getProductOrder(queryStrings);
+  res.json(products);
+});
+
+router.get('/products/filtros', async (req, res) => {
+    const queryStrings = req.query;
+    const products = await getProductLimitPage(queryStrings);
+    res.json(products);
+})
+
+module.exports = router;
